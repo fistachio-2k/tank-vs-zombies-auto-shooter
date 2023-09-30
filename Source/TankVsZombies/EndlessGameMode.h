26e7744cfdf759d3fa0f22636b24fa1996950b71
DorGd <dor.gid00@gmail.com>
2023-09-30 21:06:05 +0300

    create health component + EndlessGameMode

---
+++ Source/TankVsZombies/EndlessGameMode.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EndlessGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKVSZOMBIES_API AEndlessGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	void ActorDied(AActor* Actor);
+};

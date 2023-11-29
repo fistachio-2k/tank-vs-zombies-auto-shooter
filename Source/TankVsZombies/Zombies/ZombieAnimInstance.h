fac8b04d43493bfcf6a4b873fda9c3f6c2b31501
DorGd <dor.gid00@gmail.com>
2023-11-29 22:13:36 +0200

    added zombie classes

---
+++ Source/TankVsZombies/Zombies/ZombieAnimInstance.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ZombieAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKVSZOMBIES_API UZombieAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	UFUNCTION(BlueprintGetter, meta = (BlueprintThreadSafe))
+	float GetSpeed();
+	
+	UFUNCTION(BlueprintGetter, meta = (BlueprintThreadSafe))
+	float GetAngle();
+
+	UPROPERTY(BlueprintGetter=GetSpeed)
+	float Speed;
+
+	UPROPERTY(BlueprintGetter=GetAngle)
+	float Angle;
+};
---
+++ Source/TankVsZombies/Zombies/ZombieCharacter.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ZombieCharacter.generated.h"
+
+UCLASS()
+class TANKVSZOMBIES_API AZombieCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AZombieCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	TSubclassOf<AActor>
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Weapon;
+
+	UPROPERTY(EditAnywhere)
+	FName WeaponSocket = "WeaponSocket_R";
+};

77818763d75b65a72ffa1fe96798f2ec7d06ff6d
DorGd <dor.gid00@gmail.com>
2024-03-13 21:20:28 +0200

    move montage to animation dir + add root comp to weapon base

--- Source/TankVsZombies/Weapons/MeleeWeapon.cpp
+++ Source/TankVsZombies/Weapons/MeleeWeapon.cpp
@@ -13,6 +13,7 @@ AMeleeWeapon::AMeleeWeapon()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SetRootComponent(Mesh);
 }
 
 // Called when the game starts or when spawned
